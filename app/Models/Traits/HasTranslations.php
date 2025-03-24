7101325f9498a0e490bdc328ab605b1dfb7b7804
Moutasem0964 <moutasem.qassas@gmail.com>
2025-03-24 04:07:38 +0300

    definig_policies

--- app/Models/Traits/HasTranslations.php
+++ app/Models/Traits/HasTranslations.php
@@ -17,27 +17,27 @@ trait HasTranslations
     /**
      * Get a specific translation for a field.
      */
-    public function getTranslation($field, $locale)
-    {
-        return $this->translations()
-            ->where('field', $field)
-            ->where('locale', $locale)
-            ->first();
-    }
+    // public function getTranslation($field, $locale)
+    // {
+    //     return $this->translations()
+    //         ->where('field', $field)
+    //         ->where('locale', $locale)
+    //         ->first();
+    // }
 
     /**
      * Create or update a translation for a field.
      */
-    public function setTranslation($field, $locale, $value)
-    {
-        $translation = $this->translations()
-            ->where('field', $field)
-            ->where('locale', $locale)
-            ->firstOrNew();
+    // public function setTranslation($field, $locale, $value)
+    // {
+    //     $translation = $this->translations()
+    //         ->where('field', $field)
+    //         ->where('locale', $locale)
+    //         ->firstOrNew();
 
-        $translation->field = $field;
-        $translation->locale = $locale;
-        $translation->value = $value;
-        $translation->save();
-    }
+    //     $translation->field = $field;
+    //     $translation->locale = $locale;
+    //     $translation->value = $value;
+    //     $translation->save();
+    // }
 }
