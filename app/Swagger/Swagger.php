1c7f352bd74d5daf8760eed56a5272602c4fbb1e
Moutasem0964 <moutasem.qassas@gmail.com>
2025-04-24 16:08:01 +0300

    Fix casing of Swagger.php for Linux deployment

---
+++ app/Swagger/Swagger.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Reservation test",
+ *     version="1.0.0",
+ *     description="This is the API documentation for my Laravel application",
+ *     @OA\Contact(
+ *         email="moutasem.qassas@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ */
+class Swagger
+{
+    // This class is used to hold the Swagger annotations
+}
