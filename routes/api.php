c5a1affa419edf4677140edcef88fbbca08ba75e
Moutasem0964 <moutasem.qassas@gmail.com>
2025-04-24 15:31:23 +0300

    test_route

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!'],200);
+});
+
+
 
 
 
